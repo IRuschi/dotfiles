ba831ab50bf1af2ddf3e8c766ede574a334e44c0
Nico Ruschmann <run9bue@bosch.com>
2024-12-03 16:15:32 +0100

    remove monitors

--- link.sh
+++ link.sh
@@ -25,7 +25,6 @@ done
 mkdir -p "$HOME/.config/lvim/spell"
 
 files_to_link=(
-    ".config/monitors.xml"
     ".config/starship.toml"
     ".config/lvim/config.lua"
     ".config/gSnap/layouts.json"
