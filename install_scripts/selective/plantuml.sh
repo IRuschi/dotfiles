f6c7fc4e0076c3b434b2d8c981a87b868dff36f6
Ruschmann Nico (BueP/MFD1) <run9bue@bosch.com>
2025-01-15 09:51:15 +0100

    install feh

--- install_scripts/selective/plantuml.sh
+++ install_scripts/selective/plantuml.sh
@@ -4,3 +4,4 @@ sudo apt install graphviz
 wget https://netcologne.dl.sourceforge.net/project/plantuml/plantuml.jar -O /tmp/plantuml.jar
 sudo mkdir -p /usr/local/share/plantuml && cp /tmp/plantuml.jar /usr/local/share/plantuml/plantuml.jar
 sudo sed -i 's/\/usr\/share/\/usr\/local\/share/' /usr/bin/plantuml
+sudo apt-get install feh
