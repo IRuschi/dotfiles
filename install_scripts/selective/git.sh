50ef039a23a4f25b60e7301b71c2e593b14cbdbb
Nico Ruschmann <run9bue@bosch.com>
2025-02-19 18:08:32 +0100

    add git user config

--- install_scripts/selective/git.sh
+++ install_scripts/selective/git.sh
@@ -8,3 +8,5 @@ git config --global bash.showDirtyState true &&
 git config --global bash.showUntrackedFiles true &&
 git config --global pull.rebase true &&
 git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
+git config --global user.email "run9bue@bosch.com"
+git config --global user.name "Nico Ruschmann"
